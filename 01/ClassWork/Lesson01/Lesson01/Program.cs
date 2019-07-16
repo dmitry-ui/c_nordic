ff9a2d75e531a64a2f4db838efa6d84a202410ed
dmitry-ui <dmit.abram0w@yandex.ru>
2019-07-16 23:01:31 +0300

    less01_homework

---
+++ 01/ClassWork/Lesson01/Lesson01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+
+namespace Lesson01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //для решения проблем с кодировкой
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            ///////////////////////////////////////////
+            string str;
+            Console.WriteLine("Введите  свое имя:");
+            str = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine("Ваше имя: {0}", str);
+            Console.WriteLine($"Ваше имя: {str}");
+            Console.ReadKey();
+        }
+    }
+}
