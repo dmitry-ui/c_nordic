492365da9d5eda80883b5f34f88a4153407dfb23
Dmitry <dmit.abram0w@yandex.ru>
2019-07-17 22:09:40 +0300

    df

--- Lesson02/Lesson02/Program.cs
+++ Lesson02/Lesson02/Program.cs
@@ -6,58 +6,75 @@ namespace Lesson02
     {
         static void Main(string[] args)
         {
-            int b;
-            b = 5;
-            int a=10;
-            Int32 c = 15;// тот же int
-            System.Int32 d = 20;// тот же int
-            string name = "Некоторая строка";
-            char ch = 'r';
-            var myvar = 15;
-            var myvar1 = "строка";
+            //int b;
+            //b = 5;
+            //int a=10;
+            //Int32 c = 15;// тот же int
+            //System.Int32 d = 20;// тот же int
+            //string name = "Некоторая строка";
+            //char ch = 'r';
+            //var myvar = 15;
+            //var myvar1 = "строка";
 
-            //форматы целых чисел
-            byte maxBite = 255; //от 0 до 255
-            sbyte maxSignedBite=SByte.MaxValue; // от -128 до 127
-            sbyte minSignedBite = SByte.MinValue;
+            ////форматы целых чисел
+            //byte maxBite = 255; //от 0 до 255
+            //sbyte maxSignedBite=SByte.MaxValue; // от -128 до 127
+            //sbyte minSignedBite = SByte.MinValue;
 
-            byte ageHex = 0x24;
-            Console.WriteLine(ageHex);
-
-
-            //два байта
-            short df = -21;
-
-            //плав точка
+            //byte ageHex = 0x24;
+            //Console.WriteLine(ageHex);
 
 
-            int sun=1_321_123;
-            long univers = sun;
+            ////два байта
+            //short df = -21;
 
-            //ошибка
-            //sun=univers
+            ////плав точка
 
-            //надо так
-            sun = (int)univers;
-            //в строку можно превратить все:
-            string valueOfSun = sun.ToString();
-            Console.WriteLine("valueOfSun={0}", valueOfSun);
-            Console.WriteLine(sun);
 
-            //если надо записать в int  с клавиатуры:
-            Console.WriteLine("Введите число:");
+            //int sun=1_321_123;
+            //long univers = sun;
+
+            ////ошибка
+            ////sun=univers
+
+            ////надо так
+            //sun = (int)univers;
+            ////в строку можно превратить все:
+            //string valueOfSun = sun.ToString();
+            //Console.WriteLine("valueOfSun={0}", valueOfSun);
+            //Console.WriteLine(sun);
+
+            ////если надо записать в int  с клавиатуры:
+            //Console.WriteLine("Введите число:");
+            //string value = Console.ReadLine();
+            //int intVal = int.Parse(value); //  Parse  -  что мы парсим чтобы получить int
+
+            //Console.WriteLine(value + value);
+            //Console.WriteLine(intVal + intVal);
+
+            //Console.WriteLine("Hello World! {0}/{1}={2}", a, b, a/b);
+            //Console.WriteLine($"{a}*{b}={a * b}");
+            //Console.WriteLine(a * b*c*d);
+            //Console.WriteLine(name);
+            //Console.WriteLine(myvar1);
+            //var q = Console.ReadKey();
+
+
+            int first, second;
+            Console.WriteLine("Введите первое число:");
             string value = Console.ReadLine();
-            int intVal = int.Parse(value); //  Parse  -  что мы парсим чтобы получить int
+            first = int.Parse(value); //  Parse  -  что мы парсим чтобы получить int
 
-            Console.WriteLine(value + value);
-            Console.WriteLine(intVal + intVal);
-                                 
-            Console.WriteLine("Hello World! {0}/{1}={2}", a, b, a/b);
-            Console.WriteLine($"{a}*{b}={a * b}");
-            Console.WriteLine(a * b*c*d);
-            Console.WriteLine(name);
-            Console.WriteLine(myvar1);
-            var q = Console.ReadKey();
+            Console.WriteLine("Введите второе число:");
+            string value1 = Console.ReadLine();
+            second = int.Parse(value1); //  Parse  -  что мы парсим чтобы получить int
+
+            Console.WriteLine("{0}+{1}={2}", first, second, second + first);
+
+
+
+
+
 
             Console.ReadKey();
         }
