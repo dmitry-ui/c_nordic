ecab553b081ff7295f9b95f333cc6493182b5171
Dmitry <dmit.abram0w@yandex.ru>
2019-07-17 20:42:09 +0300

    Project files for lesson #2

---
+++ Lesson02/Lesson02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            b = 5;
+            int a=10;
+            Int32 c = 15;// тот же int
+            string name = "Некоторая строка";
+            char ch = 'r';
+            var myvar = 15;
+            var myvar1 = "строка";
+            
+
+            Console.WriteLine("Hello World! {0}/{1}={2}", a, b, a/b);
+            Console.WriteLine($"{a}*{b}={a * b}");
+            Console.WriteLine(a * b);
+            Console.WriteLine(name);
+            var q = Console.ReadKey();
+
+            Console.ReadKey();
+        }
+    }
+}
