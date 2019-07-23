ecb7b089a965f828e4985f1564ab56f0924722d0
dmitry-ui <dmit.abram0w@yandex.ru>
2019-07-23 23:40:38 +0300

    осваиваем наследование

--- Projects/Nasledovanie/Nasledovanie/Nasledovanie/CodeFile1.cs
+++ Projects/Nasledovanie/Nasledovanie/Nasledovanie/CodeFile1.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 class TwoDimFigure
 {
+    //переменные класса
     private int a;
     private int b;
+    //доступ к закрытым членам класса через свойства
+    //свойства на переменные класса
     public int PropA
     {
-            get { return a; }
-            set { a = value < 0 ? -value : value;
- }
+            get { return a;}
+            set { a = value < 0 ? -value : value;}
         
     }
     public int PropB
@@ -15,6 +17,7 @@ class TwoDimFigure
         get { return b; }
         set { b = value < 0 ?-value: value; }
     }
+    //функции 
     public int GetA()
     {
         return a;
@@ -33,25 +36,49 @@ class TwoDimFigure
         Console.WriteLine("Длина фигуры: {0}, ширина фигуры: {1}.",PropA,PropB);
     }
 }
-
 class Triangle: TwoDimFigure
 {
+    //gtht
+    public string typeOffTriangle;
+
+    //функции
+    public void ShowType()
+    {
+        Console.WriteLine("Тип треугольника: {0}", this.typeOffTriangle);
+    }
     public void Square()
     {
         Console.WriteLine("Площадь треугольника: {0}.", PropA*PropB/2);
     }
 }
+class Rectangle:TwoDimFigure
+{
+    public void Area()
+    {
+        Console.WriteLine("Площадь прямоугольника: {0}", PropA * PropB);
+        //Console.WriteLine("Площадь прямоугольника: {0}", a * b);
+    }
 
+}
 class Start
 {
     static void Main()
     {
         TwoDimFigure fig = new TwoDimFigure();
         fig.Show();
-
+        
+        //треугольник
         Triangle tri = new Triangle();
+        tri.typeOffTriangle = "Тупой";
         tri.Show();
         tri.Square();
+        tri.ShowType();
+        
+        //прямоугольник
+        Rectangle rec = new Rectangle();
+        rec.Area();
+
+        Console.ReadKey();
     }
 
 }
