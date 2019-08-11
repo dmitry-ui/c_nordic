9e1d794190827683d98e93e9f458fa31b27d1faa
dmitry-ui <dmit.abram0w@yandex.ru>
2019-08-11 09:50:32 +0300

    TryParse

---
+++ TryParse/TryParse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //контроль введенного числа с клавиатуры
+            int val1;
+            Console.WriteLine("Введите число:");
+            string str1;
+            str1 = Console.ReadLine();
+            bool ok;
+            ok = int.TryParse(str1, out val1);
+            if (!ok)
+            {
+                do
+                {
+                    Console.WriteLine("Вы ввели некорректное число, повторите попытку:");
+                    str1 = Console.ReadLine();
+                    ok = int.TryParse(str1, out val1);
+                }
+                while (!ok);
+            }
+
+            Console.WriteLine("Вы ввели число {0}", val1);
+
+            Console.ReadKey();
+        }
+    }
+}
