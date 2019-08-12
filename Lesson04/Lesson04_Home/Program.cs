08642ea3be71a37be268616a41038f47c8f39b19
dmitry-ui <dmit.abram0w@yandex.ru>
2019-08-12 07:24:40 +0300

    12

--- Lesson04/Lesson04_Home/Program.cs
+++ Lesson04/Lesson04_Home/Program.cs
@@ -6,7 +6,7 @@ namespace Lesson04_Home
     {
         //задаем палитру
        [Flags]
-        enum  ColorsOf: byte
+        enum  ColorsOf
         {
             None=0,
             Black=1,
@@ -16,28 +16,30 @@ namespace Lesson04_Home
             Green = 1<<4,
             Magenta = 1<<5,
             Red = 1<<6,
-            White = 1<<7
-           // Yellow = 9
+            White = 1<<7,
+            Yellow = 1<<8
         }
 
         static void Main(string[] args)
         {
             ColorsOf FavouriteColors =ColorsOf.None;
-            //ColorsOf AllColors;
-            //AllColors = ColorsOf.Black | ColorsOf.Blue | ColorsOf.Cyan | ColorsOf.Green | ColorsOf.Grey | ColorsOf.Magenta | ColorsOf.Red | ColorsOf.White;
-            //ColorsOf tempColorsOf = ColorsOf.Black;
-            //for (tempColorsOf = ColorsOf.Black; tempColorsOf <= ColorsOf.White; tempColorsOf++)
-            //    Console.WriteLine("{0} - {1}", tempColorsOf, (byte)tempColorsOf);
-
+            ColorsOf AllColors;
+            AllColors = ColorsOf.Black | ColorsOf.Blue | ColorsOf.Cyan | ColorsOf.Green | ColorsOf.Grey | ColorsOf.Magenta | ColorsOf.Red | ColorsOf.White | ColorsOf.Yellow ;
+            //AllColors = 0b111111111;
+           
             //вывод всей коллекции
-            Console.WriteLine("\nВся коллекция:");
-            foreach (string i in Enum.GetNames(typeof(ColorsOf)))
-                Console.WriteLine(i);
+            //Console.WriteLine("\nВся коллекция:");
+            //foreach (string i in Enum.GetNames(typeof(ColorsOf)))
+            //    Console.WriteLine(i);
 
-            Console.WriteLine("\nИх коды(соответственно):");
-            foreach (byte i in Enum.GetValues(typeof(ColorsOf)))
-                Console.WriteLine(i);
-            
+            //Console.WriteLine("\nИх коды(соответственно):");
+            //foreach (byte i in Enum.GetValues(typeof(ColorsOf)))
+            //    Console.WriteLine(i);
+
+            Console.WriteLine("\nВся коллекция:6");
+            foreach (int i in Enum.GetValues(typeof(ColorsOf)))
+                Console.WriteLine($"{(ColorsOf)i}  -  {i}");
+
 
             //выбираем 4 любимых цвета
             //надо бы TryParse
@@ -54,7 +56,10 @@ namespace Lesson04_Home
             Console.WriteLine(FavouriteColors);
 
             //получение и вывод остальных цветов
-            ColorsOf NotFavouriteColors = ~FavouriteColors;
+            ColorsOf NotFavouriteColors = ~FavouriteColors; //инверсия
+            NotFavouriteColors = NotFavouriteColors & AllColors; //очищаем старшие разряды в которых нет значение
+            //или так
+            //NotFavouriteColors = NotFavouriteColors;
             Console.WriteLine("\nОстальные цвета:");
             Console.WriteLine(NotFavouriteColors);
             
