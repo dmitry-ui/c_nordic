b272bae0c04d6633dc8ce4592f8c02279e5f4c44
ivanavatkov <vanes700i@yandex.ru>
2019-08-21 20:51:23 +0300

    1

---
+++ Lesson08/Lesson08/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson08_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<double> doubleList = new List<double>();
+			const string stop = "stop";
+			string inputString = String.Empty;
+			while (true)
+			{
+				inputString = Console.ReadLine();
+				if (inputString == stop)
+				{
+					break;
+				}
+				double doubleVal = Double.Parse(inputString);
+				doubleList.Add(doubleVal);
+			}
+
+			////сумма элементов
+			double count = 0;
+			foreach (double temp in doubleList)
+				count += temp;
+			Console.WriteLine(count);
+			Console.WriteLine(count / doubleList.Count);
+
+
+
+
+
+
+
+
+
+
+
+			Console.ReadLine();
+		}
+	}
+}
