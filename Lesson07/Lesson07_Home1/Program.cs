8d3a72adbbd86b3ee537ea3a171475b778ee2a64
ivanavatkov <vanes700i@yandex.ru>
2019-08-21 18:49:43 +0300

    1

---
+++ Lesson07/Lesson07_Home1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson07_Home1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//запросить строку, ввод строки оканчивается после ввода слова exit
+			//вывести количество слов на букву А
+
+
+
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
