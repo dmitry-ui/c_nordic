0fd17b8c922a1d59b8891e286771abfb45dfbdce
dmitry-ui <dmit.abram0w@yandex.ru>
2019-08-24 08:38:48 +0300

    1

---
+++ Lesson07/Lesson07_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson07_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
