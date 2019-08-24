b040172b80978117caf3eb58d6426cc7f2535556
dmitry-ui <dmit.abram0w@yandex.ru>
2019-08-24 07:56:52 +0300

    12

---
+++ Lesson07/Lesson07_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson07_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дана строка с “грязными” пробелами, например:
+            //string text = " lorem ipsum dolor sit amet ";
+            //Необходимо произвести над ней следующие операции:
+            //1. “Очистить” исходную строку от лишних пробелов в начале, в конце
+            //строки, а также между словами, а также поднять регистр второго слова:
+            //○ “ lorem ipsum dolor sit amet ” → “lorem IPSUM dolor sit amet”
+            //2.Удалить из исходной строки последнее слово и пробелы перед ним:
+            //○ “ lorem ipsum dolor sit amet ” → “ lorem ipsum dolor sit”
+            //Результаты по каждому пункту вывести отдельной строкой.Пример:
+            //lorem IPSUM dolor sit amet
+            // lorem ipsum dolor sit
+
+        }
+    }
+}
