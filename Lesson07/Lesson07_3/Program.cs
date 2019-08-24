1fb643149847e6aa1af8ce5f3825ebf21ca56f95
dmitry-ui <dmit.abram0w@yandex.ru>
2019-08-24 08:30:43 +0300

    2

--- Lesson07/Lesson07_3/Program.cs
+++ Lesson07/Lesson07_3/Program.cs
@@ -18,6 +18,30 @@ namespace Lesson07_3
             //lorem IPSUM dolor sit amet
             // lorem ipsum dolor sit
 
+            
+
+            string text = "  lorem ipsum       dolor sit amet ";
+            string[] words = text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            words[1] = words[1].ToUpper();
+            string text1 = string.Empty;
+            string text2 = string.Empty;
+            
+            //по 1 пункту задания
+            for (int i = 0; i < words.Length; i++)
+                text1 = text1 + words[i] + " ";
+            text1 = text1.TrimEnd();
+
+            //по 2 пункту задания
+            words[1] = words[1].ToLower();
+            for (int i = 0; i < words.Length-1; i++)
+                text2 = text2 + words[i] + " ";
+            text2 = text2.TrimEnd();
+
+            //все на выход
+            Console.WriteLine(text);
+            Console.WriteLine(text1);
+            Console.WriteLine(text2);
+            Console.ReadKey();
         }
     }
 }
