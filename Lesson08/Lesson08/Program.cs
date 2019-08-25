43476de822c40659ad2c236d3b1bb9c7352776a4
dmitry-ui <dmit.abram0w@yandex.ru>
2019-08-25 23:10:41 +0300

    ап

--- Lesson08/Lesson08/Program.cs
+++ Lesson08/Lesson08/Program.cs
@@ -7,99 +7,130 @@ namespace Lesson08_2
 	{
 		static void Main(string[] args)
 		{
-			////связанные списки
-			//List<double> doubleList = new List<double>();
-			//const string stop = "stop";
-			//string inputString = String.Empty;
-			//while (true)
-			//{
-			//	inputString = Console.ReadLine();
-			//	if (inputString == stop)
-			//	{
-			//		break;
-			//	}
-			//	double doubleVal = Double.Parse(inputString);
-			//	doubleList.Add(doubleVal);
-			//}
+            ////связанные списки
+            List<double> doubleList = new List<double>();
+            const string stop = "stop";
+            string inputString = String.Empty;
+            while (true)
+            {
+                inputString = Console.ReadLine();
+                if (inputString == stop)
+                {
+                    break;
+                }
+                double doubleVal = Double.Parse(inputString);
+                doubleList.Add(doubleVal);
+            }
 
-			//////сумма элементов и среднее арифметическое
-			//double count = 0;
-			//foreach (double temp in doubleList)
-			//	count += temp;
-			//Console.WriteLine(count);
-			//Console.WriteLine(count / doubleList.Count);
+            ////сумма элементов и среднее арифметическое
+            double count = 0;
+            foreach (double temp in doubleList)
+                count += temp;
+            Console.WriteLine(count);
+            Console.WriteLine(count / doubleList.Count);
 
-			//словари
-			//Dictionary<int, string> countries = new Dictionary<int, string>(5);
-			//countries.Add(1, "Russia");
-			//countries.Add(2, "Grate Britan");
-			//countries.Add(3, "USA");
-			//countries.Add(4, "France");
-			//countries.Add(5, "China");
+            //словари
+            //Dictionary<int, string> countries = new Dictionary<int, string>(5);
+            //countries.Add(1, "Russia");
+            //countries.Add(2, "Grate Britan");
+            //countries.Add(3, "USA");
+            //countries.Add(4, "France");
+            //countries.Add(5, "China");
 
-			//foreach (KeyValuePair<int, string> keyValue in countries)
-			//	Console.WriteLine($"{keyValue.Key} - {keyValue.Value}");
-			//string country = countries[4];  //4- это ключ
-			//countries[4] = "Spain";
-			//countries.Remove(2);
-			//Console.WriteLine();
-			//foreach (KeyValuePair<int, string> keyValue in countries)
-			//	Console.WriteLine($"{keyValue.Key} - {keyValue.Value}");
-
-
-			//Dictionary<string, string> countries1 = new Dictionary<string, string>(5);
-			//countries1.Add("1qw", "Russia");
-			//countries1.Add("2qw", "Grate Britan");
-			//countries1.Add("3ee", "USA");
-			//countries1.Add("4w", "France");
-			//countries1.Add(5.ToString(), "China");
-			//foreach (KeyValuePair<string, string> keyValue in countries1)
-			//	Console.WriteLine($"{keyValue.Key} - {keyValue.Value}");
-			//string country1 = countries1["4w"];  //4w- это ключ
-
-			//Console.WriteLine(" f fg  f g f gfgf  {0} fgdg  fd g fdg  {0}", countries1["4w"]);
+            //foreach (KeyValuePair<int, string> keyValue in countries)
+            //	Console.WriteLine($"{keyValue.Key} - {keyValue.Value}");
+            //string country = countries[4];  //4- это ключ
+            //countries[4] = "Spain";
+            //countries.Remove(2);
+            //Console.WriteLine();
+            //foreach (KeyValuePair<int, string> keyValue in countries)
+            //	Console.WriteLine($"{keyValue.Key} - {keyValue.Value}");
 
 
+            //Dictionary<string, string> countries1 = new Dictionary<string, string>(5);
+            //countries1.Add("1qw", "Russia");
+            //countries1.Add("2qw", "Grate Britan");
+            //countries1.Add("3ee", "USA");
+            //countries1.Add("4w", "France");
+            //countries1.Add(5.ToString(), "China");
+            //foreach (KeyValuePair<string, string> keyValue in countries1)
+            //	Console.WriteLine($"{keyValue.Key} - {keyValue.Value}");
+            //string country1 = countries1["4w"];  //4w- это ключ
 
-
-			Stack<string> type = new Stack<string>();
-			type.Push("wash");
-
-			while (type.Count>0)
-			{
-				string inpitValue = Console.ReadLine();
-				if (inpitValue == "wash")
-					type.Push("wash");
-				if (inpitValue == "dry")
-					type.Pop();
-			}
-			Console.WriteLine("стопка пуста");
+            //Console.WriteLine(" f fg  f g f gfgf  {0} fgdg  fd g fdg  {0}", countries1["4w"]);
 
 
 
 
-			//foreach (string number in numbers)
-			//{
-			//	Console.WriteLine(number);
-			//}
-			//while (numbers.Count > 0)
-			//{
-			//	string n = numbers.Pop();
-			//	Console.Write($"Processing \"{n}\"... ");
-			//	// here we can really do something with popped element :)
-			//	Console.WriteLine("OK");
-			//}
+            //Stack<string> type = new Stack<string>();
+            //type.Push("wash");
+
+            //while (type.Count>0)
+            //{
+            //	string inpitValue = Console.ReadLine();
+            //	if (inpitValue == "wash")
+            //		type.Push("wash");
+            //	if (inpitValue == "dry")
+            //		type.Pop();
+            //}
+            //Console.WriteLine("стопка пуста");
+
+
+
+
+            //foreach (string number in numbers)
+            //{
+            //	Console.WriteLine(number);
+            //}
+            //while (numbers.Count > 0)
+            //{
+            //	string n = numbers.Pop();
+            //	Console.Write($"Processing \"{n}\"... ");
+            //	// here we can really do something with popped element :)
+            //	Console.WriteLine("OK");
+            //}
+
+
+
+            ////списки
+            ////ввести неско-ко значений и вывести их среднее
+            //List<double> MyListDouble = new List<double>();
+            //Console.WriteLine("Введите несколько значений, а затем stop:");
+            //do
+            //{
+            //    string inputStr = Console.ReadLine();
+            //    double inputDouble;
+            //    if (inputStr == "stop")
+            //        break;
+            //    try
+            //    {
+            //        inputDouble = double.Parse(inputStr);
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine("Неверные данные, повторите ввод:");
+            //        Console.WriteLine(e.Message);
+            //        continue;
+            //    }
+            //    MyListDouble.Add(inputDouble);
+            //}
+            //while (true);
+
+            //double Total = 0;
+            //double Avg;
+            //int Quantity;
+            //foreach (double tempDouble in MyListDouble)
+            //    Total += tempDouble;
+            //Quantity = MyListDouble.Count;
+            //Avg = Total / Quantity;
+            //Console.WriteLine("Введено {0} значений, среднее значение {1}", Quantity, Avg);
 
 
 
 
 
 
-
-
-
-
-			Console.ReadKey();
+            Console.ReadKey();
 		}
 	}
 }
---
+++ Lesson08/Lesson08_Home01/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson08_Home01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //словари
+            /*
+             Написать приложение-игру:
+            ● Программа хранит небольшой список стран и соответствующих им
+            столиц
+            ● Пользователя циклически спрашивают столицу страны в случайном
+            порядке до тех пор, пока он не ошибется
+            ● Если пользователь угадал столицу, его нужно похвалить.
+            ● При ошибке, сообщаем, что пользователь ошибся и выходим из
+            приложения
+             */
+            int countCountries = 6;
+            Dictionary<int, string> Countries = new Dictionary<int, string>(countCountries);
+            //Countries.Add("Россия", "Москва");
+            //Countries.Add("Сербия", "Белград");
+            Countries.Add(1,"Австралия");
+            Countries.Add(2, "Дания");
+            Countries.Add(3, "Ирландия");
+            Countries.Add(4, "Исландия");
+            Countries.Add(5, "Гондурас");
+
+            Dictionary<int, string> Capitals = new Dictionary<int, string>(countCountries);
+            Capitals.Add(1, "Канберра");
+            Capitals.Add(2, "Копенгаген");
+            Capitals.Add(3, "Дублин");
+            Capitals.Add(4, "Рейкьявик");
+            Capitals.Add(5, "Тегусигальпа");
+
+            //Создание объекта для генерации чисел
+            Random rnd = new Random();
+
+            do
+            {
+                //Получить случайное число (в диапазоне от 1 до countCountries)
+                int rndValue = rnd.Next(1, countCountries);
+                Console.WriteLine("Введите столицу государства {0}:", Countries[rndValue]);
+                string inputStr = Console.ReadLine();
+                if (inputStr== Capitals[rndValue])
+                {
+                    Console.WriteLine("Правильно!");
+                }
+                else
+                {
+                    Console.WriteLine("К сожалению, вы ошиблись...\n");
+                    break;
+                }
+            }
+            while (true);
+            Console.WriteLine("Для закрытия программы нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
