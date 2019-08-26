a409cef55c6f567b2ad7ecc15e27e9f409461555
ivanavatkov <vanes700i@yandex.ru>
2019-08-26 21:37:41 +0300

    12

--- Lesson09/Lesson09/Program.cs
+++ Lesson09/Lesson09/Program.cs
@@ -6,8 +6,61 @@ namespace Lesson09
 	{
 		static void Main(string[] args)
 		{
-			int x = 0;
-			x = x + 1;
+			const int length = 10;
+			const int maxValue = 100;
+			int a = 10;
+			UpdateValue(ref a);   //передается по ссылке
+			Console.WriteLine(a);
+			int[] arr = GetInitialArray(length, maxValue);
+			OutputArray(arr, "Исходные данные:");
+			int[] arr1 = BubleSort(arr);
+
+			OutputArray(arr, "Результирующие данные:");
+			OutputArray(arr1, "Результирующие данные:");
+			Console.ReadKey();
+		}
+		public static int[] GetInitialArray(int arrayLenght, int maxEleventValue)
+		{
+			int[] arr = new int[arrayLenght];
+			var rnd = new Random();
+
+			for (int i = 0; i < arr.Length; i++)
+				arr[i] = rnd.Next(maxEleventValue);
+
+			return arr;
+		}
+		public static void OutputArray(int[] arr, string message)
+		{
+			Console.WriteLine(message);
+			Console.WriteLine(string.Join(",  ", arr) + "\n");
+		}
+		public static int[] BubleSort(int[] inputArr)
+		{
+			//int[] arr = new int[inputArr.Length];
+			//for(int i = 0; i<arr.Length; i++)
+			//{
+			//	arr[i] = inputArr[i];
+			//}
+
+			int[] arr = (int[])inputArr.Clone();
+
+
+			for (int j = 0; j < arr.Length - 1; j++)
+				for (int i = 0; i < arr.Length - 1 - j; i++)
+				{
+					if (arr[i] > arr[i + 1])
+					{
+						int temp = arr[i];
+						arr[i] = arr[i + 1];
+						arr[i + 1] = temp;
+					}
+				}
+			return arr;
+		}
+		public static void UpdateValue(ref int a) //передается по ссылке
+		{
+			a++;
+			Console.WriteLine(a);
 		}
 	}
 }
