8d0bdbd760538beb20abac8458ae215547e7b0e1
ivanavatkov <vanes700i@yandex.ru>
2019-08-26 19:48:31 +0300

    qwrgf

---
+++ Lesson08/Lesson08_Home02/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson08_Home02
+{
+	public enum skobki { open, close}
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			//проверить скобки
+			Stack<char> skobki = new Stack<char>();
+
+			Console.WriteLine("Введите выражение со скобками:");
+			string expression = Console.ReadLine();
+
+			foreach (char symbol in expression)
+			{
+				if (symbol == '(')
+				{
+					skobki.Push('(');
+					//Console.WriteLine(skobki.Count);
+				}
+				if (symbol == '[')
+				{
+					skobki.Push('[');
+					//Console.WriteLine(skobki.Count);
+				}
+				if (symbol == ']')
+				{
+					if(skobki.Peek() == '[')
+						skobki.Pop();
+					else
+						Console.WriteLine("Ошибки в скобках!");
+					//Console.WriteLine(skobki.Count);
+				}
+
+				if (symbol == ')')
+				{
+					if (skobki.Peek() == '(')
+						skobki.Pop();
+					else
+						Console.WriteLine("Ошибки в скобках!");
+					//Console.WriteLine(skobki.Count);
+				}
+				
+
+
+
+
+
+
+			}
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Lesson09/Lesson09/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson09
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int x = 0;
+			x = x + 1;
+		}
+	}
+}
