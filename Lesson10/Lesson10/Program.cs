33c7ed473f789776b4881bd945faab96bdfb7fba
Slayer143 <niki2025@mail.ru>
2019-08-28 22:09:20 +0300

    34

--- Lesson10/Lesson10/Program.cs
+++ Lesson10/Lesson10/Program.cs
@@ -8,32 +8,68 @@ namespace Lesson10
 		{
 			Pet Pet1 = new Pet();
 			Pet1.Age = 10;
-			Pet1.Kind = Pet.Enimals.Cat;
+			Pet1.Kind = Pet.Animals.Cat;
 			Pet1.Name = "Petty";
-			Pet1.Sex = Pet.SexOf.F;
-			Console.WriteLine($"{Pet1.Name} is a {Pet1.Kind} ({Pet1.Sex}) of {Pet1.Age} years old.");
+			Pet1.Sex = 'm';
+			Pet1.setBirthPlace("Moscow zoo");
+			Console.WriteLine(Pet1.Description);
 
 			Pet Pet2 = new Pet()
 			{
-				Age = 10,
-				Kind = Pet.Enimals.Cat,
-				Name = "Petty",
-				Sex = Pet.SexOf.F
+				Age = 5,
+				Kind = Pet.Animals.Dog,
+				Name = "Sally",
+				Sex = 'f',
 			};
-			Console.WriteLine($"{Pet2.Name} is a {Pet2.Kind} ({Pet2.Sex}) of {Pet2.Age} years old.");
+			Pet2.setBirthPlace("London zoo");
+			Console.WriteLine(Pet2.Description);
 			Console.ReadKey();
 		}
 	}
 
 	class Pet
 	{
-		public enum Enimals { Mouse, Cat, Dog }
-		public enum SexOf { M, F }
+		public enum Animals { Mouse, Cat, Dog }
+		//public enum SexOf { M, F }
 		private string _birthPlace;
-		public Enimals Kind;
+		private char sexof;
+		public Animals Kind;
 		public string Name;
-		public SexOf Sex { get; set; }
-		public int Age { get; set; }
+
+		public char Sex
+		{
+			get
+			{
+				return Sex;
+			}
+			set
+			{
+				if (Char.ToUpper(value).Equals('F') || Char.ToUpper(value).Equals('M'))
+					sexof = Char.ToUpper(value);
+				else
+					throw new Exception();
+				
+			}
+
+		}
+		public int Age
+		{ get;
+
+
+			set;
+		}
+		public string Description
+		{
+			get
+			{
+				return $"{Name} is a {Kind} ({Sex}) of {Age} years old. It was born in {_birthPlace}";
+			}
+		}
+		public void setBirthPlace(string par)
+		{
+			_birthPlace = par;
+		}
+
 	}
 
 
