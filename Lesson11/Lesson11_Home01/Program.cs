fff15d6b0f5aae7b42618d87222f91095039ecb6
dmitry-ui <dmit.abram0w@yandex.ru>
2019-09-07 20:11:43 +0300

    hj

--- Lesson11/Lesson11_Home01/Program.cs
+++ Lesson11/Lesson11_Home01/Program.cs
@@ -2,18 +2,18 @@
 
 namespace Lesson11_Home01
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-            ReminderItem [] rm = new ReminderItem[2];
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReminderItem[] rm = new ReminderItem[2];
             for (int i = 0; i <= 1; i++)
             {
-                rm[i] = new ReminderItem(DateTimeOffset.Parse("2019-09-06 18:00:00").AddDays(i*7), i + 1 +" пятницa, вечер");
-                rm[i].WriteOutItems(); 
+                rm[i] = new ReminderItem(DateTimeOffset.Parse("2019-09-06 18:00:00").AddDays(i * 7), i + 1 + " пятницa, вечер");
+                rm[i].WriteOutItems();
                 Console.WriteLine();
             }
             Console.ReadKey();
-		}
-	}
+        }
+    }
 }
--- Lesson11/Lesson11_Home01/RemainderItem.cs
+++ Lesson11/Lesson11_Home01/RemainderItem.cs
@@ -7,14 +7,17 @@ namespace Lesson11_Home01
     class ReminderItem
     {
         public DateTimeOffset AlarmDate { get; set; }  //время Х
+
         public string AlarmMessage { get; set; }    //сообщение, соответствующее будильнику
+
         public TimeSpan TimeToAlarm     // должно быть read-only, рассчитываться как AlarmDate минус текущее время
         {
             get
             {
-                return AlarmDate - DateTimeOffset.Now;     
+                return AlarmDate - DateTimeOffset.Now;
             }
         }
+
         public bool IsOutdated   //(просрочено ли событие), должно быть read-only
         {
             get
@@ -22,13 +25,16 @@ namespace Lesson11_Home01
                 return AlarmDate < DateTimeOffset.Now;
             }
         }
+
         public ReminderItem()
         { }
+
         public ReminderItem(DateTimeOffset time, string strMessage)
         {
             AlarmDate = time;
             AlarmMessage = strMessage;
         }
+
         public void WriteOutItems()
         {
             Console.WriteLine($"AlarmDate: {AlarmDate}\nAlarmMessage: {AlarmMessage}\nTimeToAlarm: {TimeToAlarm}\nIsOutdated: {IsOutdated}");
