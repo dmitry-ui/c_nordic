c694830ca3789a223f17fc830da6ada8fc65e3fb
Slayer143 <niki2025@mail.ru>
2019-09-11 19:17:46 +0300

    ва

---
+++ Lesson13/Lesson13_H/Logs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson13_H
+{
+	class Logs
+	{
+		public enum TypeMessege {info, warning, error};
+
+		public TypeMessege TM;
+
+		public Logs(TypeMessege tm)
+		{
+			TM = tm;
+		}
+
+		public string LogMessage(string str)
+		{
+			return $"Пишем в лог {TM.ToString()} сообщение: {str}";
+		}
+	}
+
+	class ConsoleLogs : Logs
+	{
+		public ConsoleLogs(TypeMessege tm) : base(tm)
+		{
+		}
+		public void Write(string str)
+		{
+			Console.WriteLine(LogMessage(str));
+		}
+
+
+	}
+}
