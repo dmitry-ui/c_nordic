9ea1147ef9d0dfc2e725978492864b79b773d751
Slayer143 <niki2025@mail.ru>
2019-09-11 22:26:01 +0300

    gh

---
+++ Lesson14/Lesson14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson14
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (ErrorList errorList = new ErrorList("Категория"))
+			{
+				errorList.Add("что-то там");
+				errorList.Add("еще раз...");
+
+				errorList.WriteToConsole();
+			}
+
+
+
+				Console.ReadKey();
+		}
+	}
+}
