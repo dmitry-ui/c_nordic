7ac0b3b20c2eb6f6201c9fed27a90916a5d8298c
dmitry-ui <dmit.abram0w@yandex.ru>
2019-09-12 07:08:36 +0300

    ва

---
+++ Lesson13/Lesson13_Home/Models/ErrorMessage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson13_Home
+{
+    public static class ErrorMessage
+    { 
+        public static string GetInfoMessage(string str)
+        {
+            return $"{DateTime.Now} <tab>Info<tab>{str}";
+        }
+
+        public static string GetWarningMessage(string str)
+        {
+            return $"{DateTime.Now} <tab>Warning<tab>{str}";
+        }
+
+        public static string GetErrorMessage(string str)
+        {
+            return $"{DateTime.Now} <tab>Error<tab>{str}";
+        }
+    }
+
+    public static class ConsoleLogWriter
+    {
+        public static void LogInfo(string message)
+        {
+            Console.WriteLine(ErrorMessage.GetInfoMessage(message));
+        }
+
+        public static void LogWarning(string message)
+        {
+            Console.WriteLine(ErrorMessage.GetWarningMessage(message));
+        }
+
+        public static void LogError(string message)
+        {
+            Console.WriteLine(ErrorMessage.GetErrorMessage(message));
+        }
+
+    }
+}
---
+++ Lesson13/Lesson13_Home/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson13_Home
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleLogWriter.LogInfo("Все ОК");
+            ConsoleLogWriter.LogWarning("Что-то не совсем так");
+            ConsoleLogWriter.LogError("Error!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
