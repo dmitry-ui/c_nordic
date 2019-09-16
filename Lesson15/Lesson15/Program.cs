b1cce3087c086c63914c7e8f2b12c0f45e0a9bc2
Slayer143 <niki2025@mail.ru>
2019-09-16 22:14:54 +0300

    ап

--- Lesson15/Lesson15/Program.cs
+++ Lesson15/Lesson15/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lesson15
 {
@@ -6,7 +7,28 @@ namespace Lesson15
 	{
 		static void Main(string[] args)
 		{
+			//int c = 10;
+			//int d = 20;
+			//Console.WriteLine($"{c}   {d}");
+			//Swapper.Swap(ref c, ref d);
+			//Console.WriteLine($"{c}   {d}");
 
+			////string aa = "aaa";
+			////string bb = "bbb";
+			////Console.WriteLine($"{aa}   {bb}");
+			////Swapper<string>.Swap(ref aa, ref bb);
+			////Console.WriteLine($"{aa}   {bb}");
+
+			//Account<int, string> a = new Account<int, string>(10, "Число");
+			//Account<string, string> str = new Account<string, string>("11", "Строка");
+			//Account<Guid, string> mguid = new Account<Guid, string>(Guid.NewGuid(), "Строка");
+
+			//a.WriteProperties();
+			//str.WriteProperties();
+			//mguid.WriteProperties();
+
+			//пример
+			List<string> l = Swapper<List<string>>.GetDefaultObject();
 
 			Console.ReadKey();
 		}
---
+++ Lesson15/Lesson15/Swapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson15
+{
+	public static class Swapper<T> where T : class, new()  // struct
+	{
+		public static void Swap(ref T a, ref T b) 
+		{
+			T temp = a;
+			a = b;
+			b = temp;
+		}
+
+		public static T GetDefaultObject()
+		{
+			return new T();
+		}
+	}
+}
