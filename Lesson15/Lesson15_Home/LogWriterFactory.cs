92a61a228e638333894ed0768996dd7302b2ccae
dmitry-ui <dmit.abram0w@yandex.ru>
2019-09-23 07:26:50 +0300

    f

--- Lesson15/Lesson15_Home/LogWriterFactory.cs
+++ Lesson15/Lesson15_Home/LogWriterFactory.cs
@@ -22,7 +22,22 @@ namespace Lesson15_Home
             { return TempObject; }
             else if (TempObject is FileLogWriter)
             { }
-
+            /*
+             Реализовать класс LogWriterFactory, который бы сам реализовывал паттерн синглтон создавал и
+выдавал экземпляры классов с позапрошлого (не синглтоны!) домашнего задания:
+● ConsoleLogWriter,
+● FileLogWriter,
+● MultipleLogWriter.
+Класс должен иметь один метод
+public ILogWriter GetLogWriter<T>(object parameters) where T : ILogWriter
+и возвращать любой из трёх возможных классов выше (сами классы не должны меняться).
+В основном потоке программы:
+● Создать по одному экземпляру класса FileLogWriter и ConsoleLogWriter.
+● Затем создать экземпляр класса MultipleLogWriter, который бы принял в конструктор
+созданные выше экземпляры FileLogWriter и ConsoleLogWriter.
+● Сделать по одной записи логов каждого типа, чтобы убедиться, что они одновременно
+пишутся и в консоль и в файл.
+             */
 
         }
     }
