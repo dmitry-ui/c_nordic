fa53ab273c2b3dc4b706182e716718cb87c7a534
dmitry-ui <dmit.abram0w@yandex.ru>
2020-03-09 18:23:11 +0300

    df

---
+++ WebApi_test/API_Cities_AppClient/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace API_Cities_AppClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ApiOperations operaions = new ApiOperations();
+
+            //получим все города
+            //ApiOperations.GetAllCities();
+            //System.Threading.Thread.Sleep(1000);
+            //Console.WriteLine();
+
+            ////получим один город
+            //ApiOperations.GetCityById(1);
+            //System.Threading.Thread.Sleep(1000);
+            //Console.WriteLine();
+
+            //добавим новый город
+            //string newCity = "{\"name\": \"Morshansk\", \"description\": \"Тамбовская область\", \"numberOfPintsOfInterest\": 150}";
+            //ApiOperations.AddCity(newCity);
+
+            //удалим город
+            ApiOperations.DeleteCity(7);
+
+            Console.WriteLine("Для завершения програмы нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+
+    public class ApiOperations
+    {
+        //выведем все города
+        public static async Task GetAllCities()
+        {
+            //Get
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:5000/cities");
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            response.Close();
+        }
+
+        //выведем город по id
+        public static async Task GetCityById(int id)
+        {
+            //Get
+            string uri = "http://localhost:5000/cities" + $"/{id}";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            response.Close();
+        }
+
+        //Добавим новый город
+        public static async Task AddCity(string newCity)
+        {
+            //post
+            WebRequest request = WebRequest.Create("http://localhost:5000/cities");
+            request.Method = "POST"; // для отправки используется метод Post
+                                     // данные для отправки
+                                     // преобразуем данные в массив байтов
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(newCity);
+            // устанавливаем тип содержимого - параметр ContentType
+            request.ContentType = "application/json";
+            // Устанавливаем заголовок Content-Length запроса - свойство ContentLength
+            request.ContentLength = byteArray.Length;
+
+            //записываем данные в поток запроса
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            WebResponse response = await request.GetResponseAsync();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            response.Close();
+            //Console.WriteLine("Запрос выполнен...");
+        }
+
+        //Удалим город
+        public static async Task DeleteCity(int id)
+        {
+            //del
+            string str = "http://localhost:5000/cities" + $"/{id}";
+            WebRequest request = WebRequest.Create(str);
+            request.Method = "Delete"; // для отправки используется метод Post
+                                       // данные для отправки
+            WebResponse response = await request.GetResponseAsync();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            response.Close();
+        }
+
+    }
+
+
+}
